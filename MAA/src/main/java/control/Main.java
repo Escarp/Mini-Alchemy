6c2d288b94eddeca7cb00339cf2ae2bcc38acf4a
Escarp <emilio.scarpellino@gmail.com>
2018-10-26 11:44:27 +0200

    Tested Lanterna functionality

--- MAA/src/main/java/control/Main.java
+++ MAA/src/main/java/control/Main.java
@@ -1,9 +1,53 @@
 package control;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println( "Hello World!" ) ;
+		DefaultTerminalFactory defaultTerminalFactory = 
+				new DefaultTerminalFactory() ;
+		Terminal terminal = null ;
+		try{
+			terminal = defaultTerminalFactory.createTerminal() ;
+					
+			terminal.setBackgroundColor(TextColor.ANSI.BLUE);
+		    terminal.setForegroundColor(TextColor.ANSI.YELLOW);
+		    
+		    terminal.putCharacter( 'H' ) ;
+		    terminal.putCharacter( 'e' ) ;
+		    terminal.putCharacter( 'l' ) ;
+		    terminal.putCharacter( 'l' ) ;
+		    terminal.putCharacter( 'o' ) ;
+		    terminal.putCharacter( ' ' ) ;
+		    terminal.putCharacter( 'W' ) ;
+		    terminal.putCharacter( 'o' ) ;
+		    terminal.putCharacter( 'r' ) ;
+		    terminal.putCharacter( 'l' ) ;
+		    terminal.putCharacter( 'd' ) ;
+		    terminal.putCharacter( '!' ) ;
+		    terminal.putCharacter( '\n' ) ;
+		    
+		    terminal.flush() ;
+		    
+		    Thread.sleep( 2000 ) ;
+		    
+		}
+		catch( Exception e ){
+			e.printStackTrace() ;
+		}
+		finally {
+		    if( terminal != null ) {
+		        try {
+		        	terminal.close() ;
+		        }
+		        catch( Exception e ){
+		        	e.printStackTrace() ;
+		        }
+		    }
+		}
 	}
 
 }
